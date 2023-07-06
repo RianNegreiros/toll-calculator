45e3618a1e2a7f125ef7622c6a80f2916951b9c1
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-07-06 19:53:02 -0300

    add aggregator service

---
+++ aggregator/service.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/RianNegreiros/toll-calculator/types"
+
+type Aggregator interface {
+	AggregateDistance(types.Distance) error
+}
+
+type Storer interface {
+	Insert(types.Distance) error
+}
+
+type InvoiceAggregator struct {
+	store Storer
+}
+
+func newInvoiceAggregator(store Storer) *InvoiceAggregator {
+	return &InvoiceAggregator{store}
+}
+
+func (i *InvoiceAggregator) AggregateDistance(distance types.Distance) error {
+	return i.store.Insert(distance)
+}
--- invoicer/service.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/RianNegreiros/toll-calculator/types"
-
-type Invoicer interface {
-	AggregateDistance(types.Distance) error
-}
